c48c60bbf986a3f0d550691d1f9c803e638cf17e
ErikHillNTIA <ehill@ntia.doc.gov>
2021-11-29 16:59:40 -0700

    Update MakeP372figs.py

--- P372/Src/AtmosPlots/MakeP372figs.py
+++ P372/Src/AtmosPlots/MakeP372figs.py
@@ -1,19 +1,26 @@
+#%% MakeP372figs.py ###########################################################
+# This script produces Figures 13a through 36c in ITU-R Recommendation P.372.
+# This script uses the data produced by ITURNoise.exe in Mode 2 as input for
+# the plots.
+
+
+#%% Imports ###################################################################
 import os
 import glob
 import numpy as np
 import cartopy.crs as ccrs
 from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 import matplotlib.pyplot as plt
-from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
-                               AutoMinorLocator)
+from matplotlib.ticker import MultipleLocator
 import matplotlib.ticker as mticker
 import pandas as pd
-
-from math import floor
 from math import ceil
+import re
+import shutil
 
-# Global ######################################################################
-# Fixed arrays used for a) Figure axies
+
+#%% Global ####################################################################
+# Fixed arrays used for a) Figure axes
 a_xtick = []
 for tick in range(-180, 190, 15):
     a_xtick.append(tick)
@@ -22,7 +29,7 @@ a_ytick = []
 for tick in range(-90, 100, 10):
     a_ytick.append(tick)
     
-# Fixed arrays used for b) and c) Figure axies
+# Fixed arrays used for b) and c) Figure axes
 bc_xtick = [ 0.01, 0.02, 0.03, 0.05, 0.07,
             0.1, 0.2, 0.3, 0.5, 0.7,
             1.0, 2.0, 3.0, 5.0, 7.0,
@@ -33,32 +40,32 @@ bc_xlabel = [ '0.01', '0.02', '0.03', '0.05', '0.07',
              '1', '2', '3', '5', '7',
              '10', '20', '30']
 
-acsvfilepath = '.\\P372_figures\\a\\csv\\'
-bcsvfilepath = '.\\P372_figures\\b\\csv\\'
-ccsvfilepath = '.\\P372_figures\\c\\csv\\'
+# Input file directories
+acsvfilepath = os.path.join(os.getcwd(), 'P372_figures', 'a', 'csv')
+bcsvfilepath = os.path.join(os.getcwd(), 'P372_figures', 'b', 'csv')
+ccsvfilepath = os.path.join(os.getcwd(), 'P372_figures', 'c', 'csv')
 
 # Output file directories
-asvgfilepath = '.\\P372_figures\\a\\svg\\'
-bsvgfilepath = '.\\P372_figures\\b\\svg\\'
-csvgfilepath = '.\\P372_figures\\c\\svg\\'
+asvgfilepath = os.path.join(os.getcwd(), 'P372_figures', 'a', 'svg')
+bsvgfilepath = os.path.join(os.getcwd(), 'P372_figures', 'b', 'svg')
+csvgfilepath = os.path.join(os.getcwd(), 'P372_figures', 'c', 'svg')
 
-apngfilepath = '.\\P372_figures\\a\\png\\'
-bpngfilepath = '.\\P372_figures\\b\\png\\'
-cpngfilepath = '.\\P372_figures\\c\\png\\'
+apngfilepath = os.path.join(os.getcwd(), 'P372_figures', 'a', 'png')
+bpngfilepath = os.path.join(os.getcwd(), 'P372_figures', 'b', 'png')
+cpngfilepath = os.path.join(os.getcwd(), 'P372_figures', 'c', 'png')
 
-apdffilepath = '.\\P372_figures\\a\\pdf\\'
-bpdffilepath = '.\\P372_figures\\b\\pdf\\'
-cpdffilepath = '.\\P372_figures\\c\\pdf\\'
+apdffilepath = os.path.join(os.getcwd(), 'P372_figures', 'a', 'pdf')
+bpdffilepath = os.path.join(os.getcwd(), 'P372_figures', 'b', 'pdf')
+cpdffilepath = os.path.join(os.getcwd(), 'P372_figures', 'c', 'pdf')
 
 
-# End Global ##################################################################
-
-def parsefilename(file):
+#%% parseFilename #############################################################
+def parseFilename(file):
     
-    mh = file.split('.')[1].split('\\')[-1].split('-')[0].split('_')[1]
-    
-    m = int(mh.split('m')[0])
-    h = int(mh.split('m')[1].split('h')[0])
+    (path,filename) = os.path.split(file)
+    matches = re.findall('\d+', filename)
+    m = int(matches[0])
+    h = int(matches[1])
     
     if m == 12 or m == 1 or m == 2:
         season = 'DEC-JAN-FEB'
@@ -84,16 +91,14 @@ def parsefilename(file):
             
     return season, time_lt
 
+
+#%% Makeafigs #################################################################
 def Makeafigs(cdafile):
+    # Process the a) file data into figures
+    print('\nCreating a figure for %s' % cdafile)
     
-    # User Feedback
-    print('MakeP372Figs: Creating a figure for %s' % cdafile)
+    season, time_lt = parseFilename(cdafile)
     
-    season, time_lt = parsefilename(cdafile)
-    
-    #******************************************************************************
-    # Process the a) file data into a Figure
-    #******************************************************************************
 
     # Pandas data frame for contour data
     dfcd = pd.read_csv(cdafile)
@@ -108,7 +113,7 @@ def Makeafigs(cdafile):
         then left-to-right (-180 to 180 degrees longitude)
         matplotlib plots assume a left-to-right then bottom-to-top
         consequently the data has to be rotated to plot
-        that is why the indecies are for zzz[yidx][xidx] and not zzz[xidx][yidx]
+        that is why the indicies are for zzz[yidx][xidx] and not zzz[xidx][yidx]
         below
     '''
     
@@ -120,8 +125,8 @@ def Makeafigs(cdafile):
             zzz[xidx][yidx] = zz[xidx + yidx*m] 
     
     # We will need a 
-    xx =  np.fromiter((i-180 for i in range(m)), float)
-    yy  =  np.fromiter((i-90 for i in range(n)), float)
+    xx = np.fromiter((i-180 for i in range(m)), float)
+    yy = np.fromiter((i-90  for i in range(n)), float)
     
     yyy,xxx = np.meshgrid(yy,xx)
     
@@ -129,13 +134,13 @@ def Makeafigs(cdafile):
     for ilvl in range(5, 120, 5):
         lvl.append(ilvl)
         
+    # Plot
     fig = plt.figure()
-    
     ax = plt.axes(projection=ccrs.PlateCarree())
     
-    CS = plt.contour(xxx,yyy,zzz,levels=lvl, cmap=plt.cm.nipy_spectral)
+    CS = plt.contour(xxx, yyy, zzz, levels=lvl, cmap=plt.cm.nipy_spectral)
     
-    plt.setp(CS.collections , linewidth=0.75)
+    plt.setp(CS.collections, linewidth=0.75)
     
     ax.set_extent([-180, 180, -90, 90], crs=ccrs.PlateCarree())
      
@@ -154,33 +159,31 @@ def Makeafigs(cdafile):
     gl.yformatter = LATITUDE_FORMATTER
     
     # Save the figures
-    outfile = asvgfilepath+'Fig_a_'+season+'_'+time_lt+'.svg'
-    print('MakeP372Figs: Saving Figure %s' % outfile)
-    plt.savefig(outfile)
+    outfile = os.path.join(asvgfilepath, 'Fig_a_' + season + '_' + time_lt + '.svg')
+    print('    Saving Figure %s' % outfile)
+    plt.savefig(outfile, bbox_inches='tight')
 
-    outfile = apngfilepath+'Fig_a_'+season+'_'+time_lt+'.png'
-    print('MakeP372Figs: Saving Figure %s' % outfile)
-    plt.savefig(outfile, dpi=300)
+    outfile = os.path.join(apngfilepath, 'Fig_a_' + season + '_' + time_lt + '.png')
+    print('    Saving Figure %s' % outfile)
+    plt.savefig(outfile, dpi=300, bbox_inches='tight')
     
-    outfile = apdffilepath+'Fig_a_'+season+'_'+time_lt+'.pdf'
-    print('MakeP372Figs: Saving Figure %s' % outfile)
-    plt.savefig(outfile, dpi=300)
+    outfile = os.path.join(apdffilepath, 'Fig_a_' +  season + '_' + time_lt + '.pdf')
+    print('    Saving Figure %s' % outfile)
+    plt.savefig(outfile, dpi=300, bbox_inches='tight')
 
     fig.clear()
     plt.close(fig)    
 
     return
 
+
+#%% Makebfigs #################################################################
 def Makebfigs(cdbfile):
+    # Process the b) file data into figures
+    print('\nCreating b figure for %s' % cdbfile)
     
-    # User Feedback
-    print('MakeP372Figs: Creating b figure for %s' % cdbfile)
+    season, time_lt = parseFilename(cdbfile)
     
-    season, time_lt = parsefilename(cdbfile)
-    
-    #******************************************************************************
-    # Process the b) file data into a Figure
-    #******************************************************************************
     
     # Pandas data frame for contour data
     dfcd = pd.read_csv(cdbfile)
@@ -200,9 +203,9 @@ def Makebfigs(cdbfile):
     z.append(dfcd['Fam100'].tolist())
     
     x = dfcd['freq'].tolist()
-        
+       
+    # Plot
     fig = plt.figure()
-    
     ax = plt.gca()
     
     ax.set_prop_cycle
@@ -210,7 +213,7 @@ def Makebfigs(cdbfile):
     # Ignore Fam5    
     for pidx in range(1, 11):
             strlabel = '%2d' % (pidx*10)
-            ax.plot(x, z[pidx],  label=strlabel, lw=0.5)
+            ax.plot(x, z[pidx], label=strlabel, lw=0.5)
         
     ax.set_xscale('log')
     
@@ -218,9 +221,9 @@ def Makebfigs(cdbfile):
     ax.yaxis.set_minor_locator(MultipleLocator(5))
     
     for ymaj in ax.yaxis.get_majorticklocs():
-      ax.axhline(y=ymaj, lw=0.5)
+      ax.axhline(y=ymaj, lw=0.5, color='k')
     for ymin in ax.yaxis.get_minorticklocs():
-      ax.axhline(y=ymin, lw=0.1)
+      ax.axhline(y=ymin, lw=0.1, color='k')
       
     plt.ylim(-20, 180)
     plt.xlim(0.01, 30.0)
@@ -230,42 +233,39 @@ def Makebfigs(cdbfile):
     
     ax.set_xticks(bc_xtick)
     ax.set_xticklabels(bc_xlabel)
-    ax.tick_params(axis='both', which='both', labelsize=6)
+    ax.tick_params(axis='both', which='both', labelsize=6, grid_color='k')
     
-    ax.legend(title='$F_{am}$ 1MHz (dB)', title_fontsize=6, fontsize=6,framealpha = 1.0)
+    ax.legend(title='$F_{am}$ 1 MHz (dB)', title_fontsize=6, fontsize=6, framealpha=1.0)
     
     plt.xlabel('Frequency (MHz)', fontsize=6)
-    plt.ylabel('$F_{am}$ (dB above k$T_0$b)', fontsize=6)
+    plt.ylabel('$F_{am}$ (dB above $kT_0B$)', fontsize=6)
     
     # Save the figures
-    outfile = bsvgfilepath+'Fig_b_'+season+'_'+time_lt+'.svg'
-    print('MakeP372Figs: Saving Figure %s' % outfile)
-    plt.savefig(outfile)
-
-    outfile = bpngfilepath+'Fig_b_'+season+'_'+time_lt+'.png'
-    print('MakeP372Figs: Saving Figure %s' % outfile)
-    plt.savefig(outfile, dpi=300)
+    outfile = os.path.join(bsvgfilepath, 'Fig_b_' + season + '_' + time_lt + '.svg')
+    print('    Saving Figure %s' % outfile)
+    plt.savefig(outfile, bbox_inches='tight')
     
-    outfile = bpdffilepath+'Fig_b_'+season+'_'+time_lt+'.pdf'
-    print('MakeP372Figs: Saving Figure %s' % outfile)
-    plt.savefig(outfile, dpi=300)
-
+    outfile = os.path.join(bpngfilepath, 'Fig_b_' + season + '_' + time_lt + '.png')
+    print('    Saving Figure %s' % outfile)
+    plt.savefig(outfile, dpi=300, bbox_inches='tight')
+    
+    outfile = os.path.join(bpdffilepath, 'Fig_b_' + season + '_' + time_lt + '.pdf')
+    print('    Saving Figure %s' % outfile)
+    plt.savefig(outfile, dpi=300, bbox_inches='tight')
+    
     fig.clear()
     plt.close(fig) 
-        
+    
     return
 
-def Makecfigs(cdcfile):
-    
-    # User Feedback
-    print('MakeP372Figs: Creating c figure for %s' % cdcfile)
-    
-    season, time_lt = parsefilename(cdcfile)
-    
-    #******************************************************************************
-    # Process the c) file data into a Figure
-    #******************************************************************************
 
+#%% Makecfigs #################################################################
+def Makecfigs(cdcfile):
+    # Process the c) file data into figures
+    print('\nCreating c figure for %s' % cdcfile)
+    
+    season, time_lt = parseFilename(cdcfile)
+    
     # Pandas data frame for contour data
     dfcd = pd.read_csv(cdcfile)
     
@@ -279,6 +279,7 @@ def Makecfigs(cdcfile):
     
     x = dfcd['freq'].tolist()
     
+    # Plot
     fig = plt.figure()
     ax = plt.gca()
     
@@ -299,102 +300,130 @@ def Makecfigs(cdcfile):
     ax.yaxis.set_minor_locator(MultipleLocator(1))
     
     for ymaj in ax.yaxis.get_majorticklocs():
-      ax.axhline(y=ymaj, lw=0.5)
+      ax.axhline(y=ymaj, lw=0.5, color='k')
     for ymin in ax.yaxis.get_minorticklocs():
-      ax.axhline(y=ymin, lw=0.1)
+      ax.axhline(y=ymin, lw=0.1, color='k')
       
     ax.grid(which='major', axis='both', linewidth=0.5)
     ax.grid(which='minor', axis='both', linewidth=0.1)
     
     ax.set_xticks(bc_xtick)
     ax.set_xticklabels(bc_xlabel)
-    ax.tick_params(axis='both', which='both', labelsize=6)
+    ax.tick_params(axis='both', which='both', labelsize=6, grid_color='k')
     
     # Set the Figure upper limit roughly 2 more than the max DuA
     cmax = (ceil(max(z[0]))-ceil(max(z[0]))%2)+2
-    plt.ylim(0,cmax)
+    plt.ylim(0, cmax)
     plt.xlim(0.01, 20.0)
     
-    ax.legend(title='$F_{am}$\nStatistics', title_fontsize=5, fontsize=5,framealpha = 1.0)
+    ax.legend(title='$F_{am}$\nStatistics', title_fontsize=5, fontsize=5, framealpha=1.0)
     
     plt.xlabel('Frequency (MHz)', fontsize=6)
     plt.ylabel('(dB)', fontsize=6)
 
     # Save the figures
-    outfile = csvgfilepath+'Fig_c_'+season+'_'+time_lt+'.svg'
-    print('MakeP372Figs: Saving Figure %s' % outfile)
-    plt.savefig(outfile)
+    outfile = os.path.join(csvgfilepath, 'Fig_c_' + season + '_' + time_lt + '.svg')
+    print('    Saving Figure %s' % outfile)
+    plt.savefig(outfile, bbox_inches='tight')
 
-    outfile = cpngfilepath+'Fig_c_'+season+'_'+time_lt+'.png'
-    print('MakeP372Figs: Saving Figure %s' % outfile)
-    plt.savefig(outfile, dpi=300)
+    outfile = os.path.join(cpngfilepath, 'Fig_c_' + season + '_' + time_lt + '.png')
+    print('    Saving Figure %s' % outfile)
+    plt.savefig(outfile, dpi=300, bbox_inches='tight')
     
-    outfile = cpdffilepath+'Fig_c_'+season+'_'+time_lt+'.pdf'
-    print('MakeP372Figs: Saving Figure %s' % outfile)
-    plt.savefig(outfile, dpi=300)
+    outfile = os.path.join(cpdffilepath, 'Fig_c_' + season + '_' + time_lt + '.pdf')
+    print('    Saving Figure %s' % outfile)
+    plt.savefig(outfile, dpi=300, bbox_inches='tight')
 
     fig.clear()
     plt.close(fig)
     return
 
-def openoutputdirectories():
-    
-    if os.path.isdir(asvgfilepath) == False:
-        os.mkdir(asvgfilepath)
-    if os.path.isdir(bsvgfilepath) == False:
-        os.mkdir(bsvgfilepath)
-    if os.path.isdir(csvgfilepath) == False:
-        os.mkdir(csvgfilepath)
-        
-    if os.path.isdir(apngfilepath) == False:
-        os.mkdir(apngfilepath)
-    if os.path.isdir(bpngfilepath) == False:
-        os.mkdir(bpngfilepath)
-    if os.path.isdir(cpngfilepath) == False:
-        os.mkdir(cpngfilepath)
 
-    if os.path.isdir(apdffilepath) == False:
-        os.mkdir(apdffilepath)
-    if os.path.isdir(bpdffilepath) == False:
-        os.mkdir(bpdffilepath)
-    if os.path.isdir(cpdffilepath) == False:
-        os.mkdir(cpdffilepath)
+#%% openOutputDirectories #####################################################
+def openOutputDirectories():
+    # If the output directories already exist, delete them, then recreate them.
+    
+    if os.path.isdir(asvgfilepath):
+        shutil.rmtree(asvgfilepath)
+    os.makedirs(asvgfilepath)
+    
+    if os.path.isdir(bsvgfilepath):
+        shutil.rmtree(bsvgfilepath)
+    os.makedirs(bsvgfilepath)
+    
+    if os.path.isdir(csvgfilepath):
+        shutil.rmtree(csvgfilepath)
+    os.makedirs(csvgfilepath)
+        
+    if os.path.isdir(apngfilepath):
+        shutil.rmtree(apngfilepath)
+    os.makedirs(apngfilepath)
+    
+    if os.path.isdir(bpngfilepath):
+        shutil.rmtree(bpngfilepath)
+    os.makedirs(bpngfilepath)
+    
+    if os.path.isdir(cpngfilepath):
+        shutil.rmtree(cpngfilepath)
+    os.makedirs(cpngfilepath)
+     
+    if os.path.isdir(apdffilepath):
+        shutil.rmtree(apdffilepath)
+    os.makedirs(apdffilepath)
+    
+    if os.path.isdir(bpdffilepath):
+        shutil.rmtree(bpdffilepath)
+    os.makedirs(bpdffilepath)
+    
+    if os.path.isdir(cpdffilepath):
+        shutil.rmtree(cpdffilepath)
+    os.mkdir(cpdffilepath)
     
     return
 
+
+#%% MakeP372figs ##############################################################
 def MakeP372figs():
-    
-    print("****************************************\n")
-    print("********** BEGIN MakeP732figs **********\n")
-    print("****************************************\n")
-    
-    if os.path.isdir(acsvfilepath):
-    
-        # Create the output directories
-        openoutputdirectories()
-        # Now make the a figures
         
-        afiles = glob.glob(acsvfilepath + '*.csv')
+    if os.path.isdir(acsvfilepath) and \
+       os.path.isdir(bcsvfilepath) and \
+       os.path.isdir(ccsvfilepath):
+           
+        # Create the output directories  
+        openOutputDirectories()
+        # Make the a figures
+        afiles = glob.glob(acsvfilepath + os.path.sep + '*.csv')
         for file in afiles:
             Makeafigs(file)
         
-        bfiles = glob.glob(bcsvfilepath + '*.csv')
+        # Make the b figures
+        bfiles = glob.glob(bcsvfilepath + os.path.sep + '*.csv')
         for file in bfiles:
             Makebfigs(file)
            
-        cfiles = glob.glob(ccsvfilepath + '*.csv')
+        # Make the c figures
+        cfiles = glob.glob(ccsvfilepath + os.path.sep + '*.csv')
         for file in cfiles:
             Makecfigs(file)
             
-        print("****************************************\n")
-        print("*********** END MakeP732figs ***********\n")
-        print("****************************************\n")
     else:
-        print('MakeP372Figs: Directory %s not accessible' % acsvfilepath)
-
+        print('\nError: one or more expected input directories do not exist.')
+        print('acsvfilepath: ' + acsvfilepath)
+        print('bcsvfilepath: ' + bcsvfilepath)
+        print('ccsvfilepath: ' + ccsvfilepath)
+        print('\nRun ITURNoise.exe in Mode 2 to produce atmospheric figure data.')
+        print('From the command prompt, run: ITURNoise.exe "path' + os.path.sep + 'to' + os.path.sep + 'CCIV' + os.path.sep + 'data"')
+        print('Exiting...')
+        
     return
 
-###############################################################################
 
+#%% Run MakeP327figs ##########################################################
 if __name__ == "__main__":
+    print("\n****************************************")
+    print("********** BEGIN MakeP732figs **********")
+    print("****************************************")
     MakeP372figs()
+    print("\n****************************************")
+    print("*********** END MakeP732figs ***********")
+    print("****************************************")
